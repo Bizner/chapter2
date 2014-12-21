dcd3bbd0a046d84a9a7d93cccb45d26607f982b5
Ngong <Ngong@10.6.2.12>
2014-12-21 10:14:12 +0700

    update

--- src/chapter2/Chapter2.java
+++ src/chapter2/Chapter2.java
@@ -17,6 +17,7 @@ public class Chapter2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Word");
+        //byte a = 127;
     }
     
 }
